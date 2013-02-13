4919450bee8f304fd9707314d0abea40bb67509c
Andy Kilner <gnublade@gmail.com>
2013-02-13 16:40:44 +0000

    Add ClientTest

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import unittest
+
 from mock import Mock, patch
+
+import cosm
 
 
 class RequestsFixtureMixin(object):
@@ -21,3 +25,22 @@ class RequestsFixtureMixin(object):
     def request(self, *args, **kwargs):
         """Returns a new mock object by default. Override in implementors."""
         return Mock()
+
+
+class ClientTest(RequestsFixtureMixin, unittest.TestCase):
+
+    def test_create(self):
+        """Tests that we can create a client object."""
+        cosm.Client("ABCDE")
+
+    def test_request_relative_url(self):
+        """Tests relative urls are requested with absolute url."""
+        client = cosm.Client("API_KEY")
+        client.request('GET', "/v2/feeds")
+        self.session.assert_called_with('GET', "http://api.cosm.com/v2/feeds")
+
+    def test_request_absolute_url(self):
+        """Tests absolute urls are requested for a different host."""
+        client = cosm.Client("API_KEY")
+        client.request('GET', "http://example.com")
+        self.session.assert_called_with('GET', "http://example.com")
